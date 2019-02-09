217c125539165c0440395bab9943948ab71487aa
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-09 21:23:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,4 @@
 def oxford_comma(array)
 comma = "kiwi"
 end
-def oxford_comma(array)
-  oxford = "kiwi" && "durian"
-end
-def oxford_comma(array)
-  comma = "kiwi", "durian","starfruit"
-end
-def oxford_comma(element)
-  comma = "kiwi", "durian","starfruit","mangos"
-end
+
