1e1ec5bed0add7af25f96b522e3701cd47e88e2e
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-09 21:20:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+comma = "kiwi"
 end
+def oxford_comma(array)
+  comma = "kiwi","durian"
+end
+def oxford_comma(array)
+  comma = "kiwi", "durian","starfruit"
+end
+def oxford_comma(array)
+  comma = "kiwi", "durian","starfruit",""
